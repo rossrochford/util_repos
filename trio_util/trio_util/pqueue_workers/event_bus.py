5873e028f44715dfe234074d0077d175a06b1aba
Ross Rochford <ross.mailinglists2@gmail.com>
2021-04-16 18:49:07 +0200

    event bus cleanup

--- trio_util/trio_util/pqueue_workers/event_bus.py
+++ trio_util/trio_util/pqueue_workers/event_bus.py
@@ -1,9 +1,7 @@
-import datetime
 import json
 import os
 import uuid
 
-import pytz
 import redis
 from redis_util.redis_trio import redis__get_pubsub_recv_iterator
 import trio
@@ -12,33 +10,30 @@ REDIS_HOSTNAME = os.environ.get('REDIS_HOSTNAME', 'localhost')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 
 
-def _get_utc_now():
-    return datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+# to publish an event use:
+# from redis_util.redis_trio import redis_publish
 
 
 class RedisEventBus(object):
 
     def __init__(self, redis_cli, redis_topic):
-        self.pending_events = {}
-        self.event_payloads = {}
+        self.event_buffer = {}
         if redis_cli is None:
             redis_cli = redis.Redis(host=REDIS_HOSTNAME, port=REDIS_PORT)
         self.redis_cli = redis_cli
         self.redis_topic = redis_topic
-        self.prev_flush_dt = None
         self.exit_daemon_flag = trio.Event()
 
     def wait_for_existing_event(self, uid):
-        if uid in self.pending_events:
-            return self.pending_events[1]
-        event = trio.Event()
-        self.pending_events[uid] = (_get_utc_now(), event)
-        return event
+        if uid not in self.event_buffer:
+            self.event_buffer[uid] = [trio.Event(), None]
+        await self.event_buffer[uid][0].wait()
 
     def create_pending_event(self):
         uid = 'pending-' + uuid.uuid4().hex[:16]
         event = trio.Event()
-        self.pending_events[uid] = (_get_utc_now(), event)
+        #                        [trio_event, event_payload]
+        self.event_buffer[uid] = [event, None]
         return uid, event
 
     async def event_received(self, event_uid, event_type, payload):
@@ -50,42 +45,22 @@ class RedisEventBus(object):
             return
 
         event_uid = msg_dict['event_uid']  # todo: use 'event_type' instead of 'pending-' prefix?
-        if event_uid.startswith('pending-') and event_uid not in self.pending_events:
+        if event_uid.startswith('pending-') and event_uid not in self.event_buffer:
             return  # no local interest in this event
 
-        now = _get_utc_now()
-
-        if msg_dict.get('event_payload'):
-            self.event_payloads[event_uid] = (now,  msg_dict['event_payload'])
-        if event_uid in self.pending_events:
-            self.pending_events[event_uid][1].set()
+        if event_uid in self.event_buffer:
+            self.event_buffer[event_uid][1] = msg_dict.get('event_payload')
+            self.event_buffer[event_uid][0].set()
 
         await self.event_received(
             event_uid, msg_dict['event_type'], msg_dict.get('event_payload')
         )
 
-        if len(self.pending_events) > 100 or len(self.event_payloads) > 100:
-            if self.prev_flush_dt is None or (now-self.prev_flush_dt).hours > 2:
-                self._flush_old_events()
-
-    def _flush_old_events(self):
-        print('flushing events')
-        now = _get_utc_now()
-        twelve_hours_ago = now - datetime.timedelta(hours=12)
-
-        to_remove = set()
-        for uid, (dt, trio_event) in self.pending_events.items():
-            if dt < twelve_hours_ago:
-                to_remove.add(uid)
-        for uid, (dt, payload) in self.event_payloads.items():
-            if dt < twelve_hours_ago:
-                to_remove.add(uid)
-        for uid in to_remove:
-            if uid in self.pending_events:
-                del self.pending_events[uid]
-            if uid in self.event_payloads:
-                del self.event_payloads[uid]
-        self.prev_flush_dt = now
+        if len(self.event_buffer) > 1000:
+            # flush old events. Note: keys are ordered by insertion time in python 3.7
+            keys_to_remove = [k for k in self.event_buffer.keys()][:300]
+            for k in keys_to_remove:
+                del self.event_buffer[k]
 
     async def receive_worker_loop(self):
         pubsub_iter = await redis__get_pubsub_recv_iterator(
